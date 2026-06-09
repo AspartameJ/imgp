b0bc143d8d6c598d1dba05fb5b308d9af00d84a3
AspartameJ <1054136235@qq.com>
2026-06-09 23:27:25 +0800

    fix: use GetConsoleMode for double-click detection

--- main.go
+++ main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"os"
+	"runtime"
+	"syscall"
+	"unsafe"
 
 	"gitcode.com/DonaldTom/imgp/cmd"
 )
@@ -17,6 +20,13 @@ func main() {
 func isDoubleClick() bool {
 	if len(os.Args) > 1 {
 		return false
+	}
+	if runtime.GOOS == "windows" {
+		var mode uint32
+		kernel32 := syscall.NewLazyDLL("kernel32.dll")
+		getConsoleMode := kernel32.NewProc("GetConsoleMode")
+		ret, _, _ := getConsoleMode.Call(uintptr(syscall.Stdout), uintptr(unsafe.Pointer(&mode)))
+		return ret == 0
 	}
 	fi, _ := os.Stdout.Stat()
 	return fi != nil && (fi.Mode()&os.ModeCharDevice) == 0
